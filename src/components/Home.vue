c58541bd4abf7fe23aa92b9dadb3a7ad5c728218
Mr.shen <cmsxiu@qq.com>
2021-05-20 13:42:37 +0800

    login功能完成

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
-    <el-button type="info">退出</el-button>
+    <el-button
+      type="info"
+      @click="logout"
+    >退出</el-button>
   </div>
 </template>
 <script>
 export default {
-
+  data () { return {} },
+  methods: {
+    logout () {
+      // window.sessionStorage.removeItem('token') 删除某一项
+      window.sessionStorage.clear() // 清空全部
+      this.$router.push('/login')
+    }
+  }
 }
 </script>
 <style scoped>
